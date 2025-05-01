89bf4f5d804198692563d4b27deddaae9892f2ab
JohanKYT <kevin.maquis@udla.edu.ec>
2025-05-01 08:45:22 -0500

    Se agrego un limitante en la base de datos para los tipo decimal y asi evitar truncamiento ademas,  se creo una nueva migracion y se cambio en migracion el tipo cascade por restrict

--- Maquis_EvaluacionProgreso1_PrograIV/Data/Maquis_DataBase_EvaluacionProgreso1_PrograIV.cs
+++ Maquis_EvaluacionProgreso1_PrograIV/Data/Maquis_DataBase_EvaluacionProgreso1_PrograIV.cs
@@ -5,16 +5,32 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Maquis_EvaluacionProgreso1_PrograIV.Models;
 
-    public class Maquis_DataBase_EvaluacionProgreso1_PrograIV : DbContext
+public class Maquis_DataBase_EvaluacionProgreso1_PrograIV : DbContext
+{
+    public Maquis_DataBase_EvaluacionProgreso1_PrograIV(DbContextOptions<Maquis_DataBase_EvaluacionProgreso1_PrograIV> options)
+        : base(options)
     {
-        public Maquis_DataBase_EvaluacionProgreso1_PrograIV (DbContextOptions<Maquis_DataBase_EvaluacionProgreso1_PrograIV> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<Maquis_EvaluacionProgreso1_PrograIV.Models.PropietarioMascota> PropietarioMascota { get; set; } = default!;
-
-public DbSet<Maquis_EvaluacionProgreso1_PrograIV.Models.Mascota> Mascota { get; set; } = default!;
-
-public DbSet<Maquis_EvaluacionProgreso1_PrograIV.Models.VisitaVeterinaria> VisitaVeterinaria { get; set; } = default!;
     }
+
+    public DbSet<Maquis_EvaluacionProgreso1_PrograIV.Models.PropietarioMascota> PropietarioMascota { get; set; } = default!;
+
+    public DbSet<Maquis_EvaluacionProgreso1_PrograIV.Models.Mascota> Mascota { get; set; } = default!;
+
+    public DbSet<Maquis_EvaluacionProgreso1_PrograIV.Models.VisitaVeterinaria> VisitaVeterinaria { get; set; } = default!;
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Mascota>()
+            .Property(m => m.Peso)
+            .HasPrecision(6, 2);
+
+        modelBuilder.Entity<PropietarioMascota>()
+            .Property(p => p.Deuda)
+            .HasPrecision(6, 2);
+
+        modelBuilder.Entity<VisitaVeterinaria>()
+            .Property(v => v.CostoMedicacion)
+            .HasPrecision(6, 2);
+    }
+
+}
+
---
+++ Maquis_EvaluacionProgreso1_PrograIV/Migrations/20250501134023_Esta es la primera migracion de datos.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Maquis_EvaluacionProgreso1_PrograIV.Migrations
+{
+    /// <inheritdoc />
+    public partial class Estaeslaprimeramigraciondedatos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PropietarioMascota",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TieneDeuda = table.Column<bool>(type: "bit", nullable: false),
+                    Deuda = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropietarioMascota", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mascota",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Raza = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Peso = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PropietarioMascotaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mascota", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Mascota_PropietarioMascota_PropietarioMascotaId",
+                        column: x => x.PropietarioMascotaId,
+                        principalTable: "PropietarioMascota",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VisitaVeterinaria",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FechaVisita = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MotivoVisita = table.Column<int>(type: "int", nullable: false),
+                    RequiereMedicacion = table.Column<bool>(type: "bit", nullable: false),
+                    CostoMedicacion = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MascotaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VisitaVeterinaria", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VisitaVeterinaria_Mascota_MascotaId",
+                        column: x => x.MascotaId,
+                        principalTable: "Mascota",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mascota_PropietarioMascotaId",
+                table: "Mascota",
+                column: "PropietarioMascotaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VisitaVeterinaria_MascotaId",
+                table: "VisitaVeterinaria",
+                column: "MascotaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VisitaVeterinaria");
+
+            migrationBuilder.DropTable(
+                name: "Mascota");
+
+            migrationBuilder.DropTable(
+                name: "PropietarioMascota");
+        }
+    }
+}
