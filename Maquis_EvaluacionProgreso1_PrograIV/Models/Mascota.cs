6920cec80965da075c95de7ef9a04b95f1814b70
JohanKYT <kevin.maquis@udla.edu.ec>
2025-05-01 08:23:34 -0500

    Se crearon los modelos del proyecto con sus respectivos atributos

---
+++ Maquis_EvaluacionProgreso1_PrograIV/Models/Mascota.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Maquis_EvaluacionProgreso1_PrograIV.Models
+{
+    public class Mascota
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+        [MaxLength(50)]
+        public string Raza { get; set; }
+        [Required]
+        public decimal Peso { get; set; } 
+        public DateTime FechaNacimiento { get; set; }
+        public int PropietarioMascotaId { get; set; }
+        [ForeignKey("PropietarioMascotaId")]
+        public PropietarioMascota? PropietarioMascota { get; set; }
+    }
+}
---
+++ Maquis_EvaluacionProgreso1_PrograIV/Models/PropietarioMascota.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Maquis_EvaluacionProgreso1_PrograIV.Models
+{
+    public class PropietarioMascota
+    {
+        [Key]
+        public int Id { get; set; }
+        [MinLength(3, ErrorMessage = "El nombre debe tener al menos 3 caracteres.")]
+        public static string NombreDelEstudiante = "Kevin Maquis";
+        public string Nombre { get; set; }
+        [Required]
+        [MaxLength(10, ErrorMessage = "El telefono no puede tener más de 10 caracteres.")]
+        public string Telefono { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "El correo electrónico no es válido.")]
+        public string Correo { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+        public bool TieneDeuda { get; set; }
+        public decimal Deuda { get; set; }
+    }
+}
