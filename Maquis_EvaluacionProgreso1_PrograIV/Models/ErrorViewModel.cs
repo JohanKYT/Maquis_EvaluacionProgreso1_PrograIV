eeb6395773c11087bd6e7f7bc2946e4ed7b21f90
JohanKYT <kevin.maquis@udla.edu.ec>
2025-05-01 07:22:33 -0500

    Add project files.

---
+++ Maquis_EvaluacionProgreso1_PrograIV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Maquis_EvaluacionProgreso1_PrograIV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
